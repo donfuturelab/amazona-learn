f456862393f7fa6931050635a5f20101f7284493
Don Huynh <donhuynh.futurelab@gmail.com>
2022-10-21 22:32:27 +0700

    Amazona-React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="https://reactjs.org">
+          Amazona
+        </a>
+      </header>
+      <main>
+        list products
+      </main>
+    </div>
+  );
+}
+
+export default App;
